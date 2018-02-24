50db0ae3ae1305ee84a96ec2e441e65932d98b0d
AnnGareeva <nana10.06@yandex.ru>
2018-02-24 23:11:07 +0300

    Add analyze errors and fix that. Was started detection lines.

--- Project/Select_of_specters/Source_2.cpp
+++ Project/Select_of_specters/Source_2.cpp
@@ -1,6 +1,8 @@
 ﻿#include <opencv2\opencv.hpp>
+#include <algorithm>
 #include <iostream>
 #include <fstream>
+
 #define  LOG_ENABLE
 #include "log.h"
 
@@ -12,23 +14,40 @@ enum Color {
 	BLUE
 };
 
+
+int BACKGROUND_DEPTH_X;
+int BACKGROUND_DEPTH_Y;
+int BACKGROUND_DEPTH_SPEC;
+
+struct SpLines {
+	int xCentr;
+	int yCentr;
+	int height;
+
+};
+
 struct Spectr {
-	int x0;
+	int x0; 
 	int y0;
 	int xCentr;
 	int yCentr;
 	int width;
 	int height;
+	vector<SpLines> lines;
+	Spectr(int x0, int y0, int xCentr, int yCentr, int width, int height) {
+		this->x0 = x0;
+		this->y0 = y0;
+		this->xCentr = xCentr;
+		this->yCentr = yCentr;
+		this->width = width;
+		this->height = height;
+	}
 };
 
 
-const static int BACKGROUND_DEPTH_X = 190;
-const static int BACKGROUND_DEPTH_Y = 210;
-
-
+// Paint Block of Functions
 void Paint(Mat src, int x, int y, Color color) {
 	Mat pix = src.col(x).row(y);
-	//log("pix", pix.at<uchar>(0, 0));
 	switch (color)
 	{
 	case RED:
@@ -43,71 +62,94 @@ void Paint(Mat src, int x, int y, Color color) {
 }
 
 void PaintRect(Mat src, Spectr sp) {
-	for (int i = sp.x0; i < sp.x0 + sp.width; i++) {
-		Mat pixUp = src.col(i).row(sp.y0);
-		//log("PixUp", pixUp);
-		pixUp -= Scalar(255, 255, 0);
-		Mat pixDown = src.col(i).row(sp.height + sp.y0);
-		//log("PixDown", pixDown);
-		pixDown -= Scalar(255, 255, 0);
+	Rect area(sp.x0, sp.y0, sp.width, sp.height);
+	rectangle(src, area, RED,3);
+}
+
+//_____________________________
+
+//Correction of ERRORS
+
+double MiddleOfBlock(int x0, int y0, Mat& imgGrey) {
+	int minX = ((x0 - 1) < 0) ? x0 : (x0 - 1);
+	int maxX = ((x0 + 1) > imgGrey.cols) ? x0 : (x0 + 1);
+	int minY = ((y0 - 1) < 0) ? y0 : (y0 - 1);
+	int maxY = ((y0 + 1) > imgGrey.rows) ? y0 : (y0 + 1);
+	int pix = 0;
+	double result = 0;
+	for (int i = minX; i < maxX; i++) {
+		for (int j = minY; j < maxY; j++) {
+			Mat curPix = imgGrey.col(i).row(j);
+			pix += curPix.at<uchar>(0, 0);
+		}
 	}
-	for (int i = sp.y0; i < sp.y0 + sp.height; i++) {
-		Mat pixLeft = src.col(sp.x0).row(i);
-		//log("PixLeft", pixLeft);
-		pixLeft -= Scalar(255, 255, 0);
-		Mat pixRight = src.col(sp.x0 + sp.width-1).row(i);
-		//log("PixRight", pixRight);
-		pixRight -= Scalar(255, 255, 0);
+	result = pix / ((maxX - minX)*(maxY - minY));
+	return result;
+}
+
+void CorrectByWidth(vector<Spectr>& spectrs) {
+	vector<Spectr> result;
+	vector<int> widths;
+	int averWidth = 0;
+	for (int i = 0; i < spectrs.size(); i++) {
+		widths.push_back(spectrs[i].width);
 	}
+	sort(widths.begin(),widths.end());
+	int idealWidth = widths[(int)widths.size() / 2];
+	//log("Ideal Width", idealWidth);
+	for (int i = 0; i < spectrs.size(); i++) {
+		if (abs(spectrs[i].width - idealWidth) > (idealWidth / 2)) {
+			spectrs.erase(spectrs.begin() + i);
+			i--;
+		}
+		
+	}
+}
+void CorrectByHeight(vector<Spectr>& spectrs) {
+	vector<Spectr> result;
+	vector<int> height;
+	int averHeight = 0;
+	for (int i = 0; i < spectrs.size(); i++) {
+		height.push_back(spectrs[i].height);
+	}
+	sort(height.begin(), height.end());
+	int idealHeight = height[(int)height.size() / 2];
+	//log("Ideal Height", idealHeight);
+	for (int i = 0; i < spectrs.size(); i++) {
+		if (abs(spectrs[i].height - idealHeight) >(idealHeight / 2)) {
+			spectrs.erase(spectrs.begin() + i);
+			i--;
+		}
+
+	}
+}
+int BgColorImg(Mat imgGrey) {
+	vector<double> bgCol;
+	int y0 = 0.01*imgGrey.rows;
+	int resBg = 255;
+	for (int i = 0; i < imgGrey.cols; i++) {
+		bgCol.push_back(MiddleOfBlock(i, y0, imgGrey));
+	}
+	resBg = *(max_element(bgCol.begin(), bgCol.end()));
+	//log("resg", resBg);
+	
+	return resBg;
 }
 
 
-// ���=����� ������� �������� �������� � �����
-//double MiddleOfBlock(int x0, int y0, Mat& imgGrey) {
-//	int pix = 0;
-//	double result = 0;
-//	for (int i = x0; i < x0 + 5; i++) {
-//		for (int j = y0; j < y0 + 2; j++) {
-//			pix += (int)imgGrey.at<uchar>(i, j);
-//		}
-//	}
-//	result = pix / 10;
-//	return result;
-//}
-
-
-
-//int SerchingLineX(int x0, int y0, Mat imgGrey, Mat&src) {
-//	
-//	while (x0 < (int)imgGrey.cols) {
-//		Mat curPix = imgGrey.col(x0).row(y0);
-//		double MVal =(int) curPix.at<uchar>(0, 0);
-//		//log("MVal", MVal);
-//		//BG
-//		if (MVal >= BACKGROUND_DEPTH_X) {
-//			Paint(src, x0, y0, RED);
-//			x0++;
-//		}
-//		//SPECTR
-//		else {
-//			Paint(src, x0, y0, BLUE);
-//			x0++;
-//		}
-//	}
-//	
-//	return x0;
-//}
-
+//______________________
 int BGX(int x0, int y0, Mat imgGrey, Mat&src) {
-
+	
+	//log("BGX", BACKGROUND_DEPTH_X);
 	while (x0 < (int)imgGrey.cols) {
-		Mat curPix = imgGrey.col(x0).row(y0);
-		double MVal = curPix.at<uchar>(0, 0);
+		//Mat curPix = imgGrey.col(x0).row(y0);
+		double MVal = MiddleOfBlock(x0, y0, imgGrey);   //curPix.at<uchar>(0, 0);
 		//log("MVal", MVal);
 		//BG
 		if (MVal >= BACKGROUND_DEPTH_X) {
-			//Paint(src, x0, y0, RED);
+			Paint(src, x0, y0, RED);
 			x0++;
+			//log("X", x0);
 		}
 		//SPECTR Start
 		else {
@@ -120,14 +162,14 @@ int BGX(int x0, int y0, Mat imgGrey, Mat&src) {
 
 
 int SpectrX(int x0, int y0, Mat imgGrey, Mat&src) {
+	
 	while (x0 < (int)imgGrey.cols) {
-		Mat curPix = imgGrey.col(x0).row(y0);
-		double MVal = curPix.at<uchar>(0, 0);
+		//Mat curPix = imgGrey.col(x0).row(y0);
+		double MVal = MiddleOfBlock(x0, y0, imgGrey);   //curPix.at<uchar>(0, 0);
 		//log("MVal", MVal);
 		//SPECTR
-
 		if (MVal < BACKGROUND_DEPTH_X) {
-			//Paint(src, x0, y0, BLUE);
+			Paint(src, x0, y0, BLUE);
 			x0++;
 		}
 		//BG
@@ -137,58 +179,18 @@ int SpectrX(int x0, int y0, Mat imgGrey, Mat&src) {
 	}
 	return x0;
 }
-//int SerchingLineY(int x0, int y0, Mat imgGrey, Mat&src) {
-//	int y = y0;
-//	while (y > 0) {
-//		Mat curPix = imgGrey.col(x0).row(y);
-//		double MVal = (int)curPix.at<uchar>(0, 0);
-//		log("MVal", MVal);
-//		//BG
-//		if (MVal < BACKGROUND_DEPTH_Y) {
-//			//Paint(src, x0, y, RED);
-//			y--;
-//		}
-//		//SPECTR
-//		else {
-//			//Paint(src, x0, y, BLUE);
-//			y--;
-//		}
-//	}
-//	return y;
-//}
-int BGY(int x0, int y0, Mat imgGrey, Mat&src) {
-	int y = y0;
-	while (y > 0) {
-		Mat curPix = imgGrey.col(x0).row(y);
-		double MVal = curPix.at<uchar>(0, 0);
-		//log("MVal", MVal);
-		//BG
-		if (MVal > BACKGROUND_DEPTH_Y) {
-			//Paint(src, x0, y0, RED);
-			y--;
-		}
-		//SPECTR Start
-		else {
-			return y;
-		}
-	}
-	return y;
-
-}
 
 
 int SpectrY(int x0, int y0, Mat imgGrey, Mat&src) {
 	int y = y0;
 	while (y > 0) {
-		Mat curPix = imgGrey.col(x0).row(y);
-		double MVal = curPix.at<uchar>(0, 0);
-		//log("MVal", MVal);
-
+		//Mat curPix = imgGrey.col(x0).row(y);
+		double MVal = MiddleOfBlock(x0, y, imgGrey);   //curPix.at<uchar>(0, 0);
+		
 		if (MVal <= BACKGROUND_DEPTH_Y) {
-			//Paint(src, x0, y0, BLUE);
+			Paint(src, x0, y0, BLUE);
 			y--;
 		}
-		//SPECTR Start
 		else {
 			return y;
 		}
@@ -196,62 +198,133 @@ int SpectrY(int x0, int y0, Mat imgGrey, Mat&src) {
 	return y;
 }
 
-void SpectrParametrs(Mat imgGrey, Mat& src) {
+vector<Spectr> DetectionOfSpectrs(Mat imgGrey, Mat& src) {
 	//log("SpectrParametrs start");
 	int x0 = 0, y0 = 0;
-	int middleY = (int)(imgGrey.rows / 2) - 20;
-	//log("middleY", middleY);
-	//log("pixStart", imgGrey.at<uchar>(x0, middleY));
+	int middleY = (int)(imgGrey.rows / 2) -20;
 	vector<Spectr> spectrsExamples;
-	
+	BACKGROUND_DEPTH_X = BgColorImg(imgGrey)*0.82;
+	BACKGROUND_DEPTH_Y = BgColorImg(imgGrey)*0.90;
+	//log("BACKGROUND_DEPTH_X", BACKGROUND_DEPTH_X);
+	//log("BACKGROUND_DEPTH_Y", BACKGROUND_DEPTH_Y);
 	while (x0 < (int)imgGrey.cols) {
-		Spectr spec;
 		int x = BGX(x0, middleY, imgGrey, src);
+		//log("X", x);
 		int x1 = SpectrX(x, middleY, imgGrey, src);
+		//log("X1", x1);
 		int centrSp = (x1 + x) / 2;
-		//log("x1", x1);
 		int yUp = SpectrY(centrSp, middleY, imgGrey, src);
-		//log("->",yUp);
-		//int y1Up = BGY(centrSp, middleY , imgGrey, src);
+		//log("YUP", yUp);
 		int yDown = SpectrY(centrSp, middleY + (middleY - yUp), imgGrey, src);
-		spec.x0 = x;
-		spec.xCentr = centrSp;
-		spec.y0 = yUp;
-		spec.yCentr = middleY;
-		spec.width = x1 - x;
-		spec.height = (middleY - yUp) * 2;
+		//log("YDOWN", yDown);
+		Spectr spec(x, yUp, centrSp, middleY, (x1 - x), ((middleY - yUp) * 2));
+		//log("Spectr");
 		spectrsExamples.push_back(spec);
-		PaintRect(src, spec);
+		//log("Paint");
 		x0 = x1;
 		y0 = yUp;
-
 	}
 	
-	
+	CorrectByWidth(spectrsExamples);
+	CorrectByHeight(spectrsExamples);
+	for (int i = 0; i < spectrsExamples.size(); i++) {
+		Spectr spec = spectrsExamples[i];
+		//log("width", spectrsExamples[i].width);
+		PaintRect(src, spec);
+		//log("Paint");
+	}
+	return spectrsExamples;
+		
+}
 
+// SpectrsLine
+int BgColorSpectr(Mat imgGrey,Spectr spec) {
+	vector<double> bgCol;
+	int resBg = 255;
+	for (int i = spec.y0+spec.height; i > spec.y0; i--) {
+		bgCol.push_back(MiddleOfBlock(spec.xCentr, i, imgGrey));
+	}
+	resBg = *(max_element(bgCol.begin(), bgCol.end()));
+	//log("resg", resBg);
 
-
-	//log("First line");
-	//int tmp = SerchingLineX(x0, middleY, imgGrey, src);
-	//x0 = tmp;
-	//
-	//tmp = SerchingLineY(x0, middleY, imgGrey, src);
-	//y0 = tmp;
-
+	return resBg;
+}
+void CompareSpLines(vector<SpLines> distLines) {
+	SpLines line;
 
 }
 
+void DetectionSpectrLines(vector<Spectr> spectrs, Mat imgGrey, Mat &src) {
+	const int countOfBlocks = 8;
+
+		for (int i = 0; i < spectrs.size(); i++) {
+			int delta= spectrs[i].width / 5;
+			int x0;
+			for (int side = 0; side < 3; side++) {
+				if( side==0){
+					 x0 = spectrs[i].xCentr-delta;
+				}
+				else {
+					if (side == 1) {
+						 x0 = spectrs[i].xCentr;
+					}
+					else {
+						 x0 = spectrs[i].xCentr + delta;
+					}
+				}
+				
+				int y1 = spectrs[i].y0 + spectrs[i].height;
+				int y0 = spectrs[i].y0;
+				int sizeOfBlock = spectrs[i].height / countOfBlocks;
+				SpLines distanceLine;
+				vector<SpLines> distanceLines;
+				distanceLine.xCentr = spectrs[i].xCentr;;
+				for (int k = countOfBlocks; k > 0; k--) {
+					Spectr block(spectrs[i].x0, y1 - sizeOfBlock,x0, y1 - (sizeOfBlock / 2), spectrs[i].width, sizeOfBlock);
+					log("IM HERE");
+					BACKGROUND_DEPTH_SPEC = BgColorSpectr(imgGrey, block)*0.8;
+					log("k", k);
+					log("BACKGROUND_DEPTH_SPEC", BACKGROUND_DEPTH_SPEC);
+					for (int j = y1; j > y1 - sizeOfBlock; j--) {
+						Mat curPix = imgGrey.col(x0).row(j);
+						//double MVal = MiddleOfBlock(x0, y0, imgGrey);   
+						double MVal = curPix.at<uchar>(0, 0);
+						//log("pix", MVal);
+						if (MVal >= BACKGROUND_DEPTH_SPEC) {
+							distanceLine.height = y1 - j;
+							distanceLine.yCentr = (y1 - j) / 2;
+							distanceLines.push_back(distanceLine);
+							Paint(src, x0, j, BLUE);
+						}
+						else {
+							Paint(src, x0, j, RED);
+							Paint(src, x0 - 1, j, RED);
+							Paint(src, x0 + 1, j, RED);
+						}
+
+					}
+					y1 -= sizeOfBlock;
+					Spectr sp(spectrs[i].x0, y1, spectrs[i].xCentr, y1 + (sizeOfBlock / 2), spectrs[i].width, sizeOfBlock);
+					PaintRect(src, sp);
+				}
+			}
+		
+	}
+}
+
+
+
 
 int main() {
-	Mat src = imread("SrcImg.png");
+	Mat src = imread("4.jpg");
 	Mat imgGrey;
 	int pix, pixNext;
 	int count = 0;
 	cvtColor(src, imgGrey, CV_BGR2GRAY);  // convert to Grey
+	vector<Spectr> detectSpectrs;
+	detectSpectrs= DetectionOfSpectrs(imgGrey, src);
+	DetectionSpectrLines(detectSpectrs,imgGrey,src);
 
-	SpectrParametrs(imgGrey, src);
-	//cout << (int) imgGrey.at<uchar>(1, 1)  << "      ";
-	//imshow("ResSRC", src);
 	imshow("ResSRC", src);
 	waitKey(0);
 	system("pause");
